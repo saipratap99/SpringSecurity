b8f8c7e9a6523551cf0f9d3d37955c512aa22e18
saipratap99 <122005067@sastra.ac.in>
2022-03-29 01:02:41 +0530

    configured for basic auth

--- src/main/java/com/example/SpringSecurity/configs/AppSecurityConfig.java
+++ src/main/java/com/example/SpringSecurity/configs/AppSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.SpringSecurity.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +11,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.httpBasic();
+	}
+
+	// We need to authorize requests, any request should be authenticated and authentication type is basic auth (we need pass username and password everytime)
+	
 	
 	
 }
