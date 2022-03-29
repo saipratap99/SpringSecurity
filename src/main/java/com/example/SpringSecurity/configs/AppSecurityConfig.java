9fcca5967735c65f9027d2c02915327b1282379c
saipratap99 <122005067@sastra.ac.in>
2022-03-29 23:44:43 +0530

    authentication using UserDetailsService - hardcoded

--- src/main/java/com/example/SpringSecurity/configs/AppSecurityConfig.java
+++ src/main/java/com/example/SpringSecurity/configs/AppSecurityConfig.java
@@ -1,11 +1,11 @@
 package com.example.SpringSecurity.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -15,6 +15,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter{
 
+	
+	@Autowired
+	UserDetailsService userDetailsService;
+	
 	// Authorization
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -35,6 +39,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter{
 	// Authentication
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
 		/* InMemoryAuthentication
 		
 		auth.inMemoryAuthentication()
@@ -48,7 +53,9 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter{
 		
 		*/
 		
-
+		/* JPA */
+		 auth.userDetailsService(userDetailsService);
+	
 		
 	}	
 	
---
+++ src/main/java/com/example/SpringSecurity/service/UserDetailsServiceImp.java
@@ -0,0 +1,19 @@
+package com.example.SpringSecurity.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.SpringSecurity.models.MyUserDetails;
+
+//To inform AutoWired
+@Service
+public class UserDetailsServiceImp implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return new MyUserDetails(username);
+	}
+
+}
